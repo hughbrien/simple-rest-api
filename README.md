f19540502580b0baa53e43f0fa8605b619f2f5ff
hughbrien <hugh@hughbrien.com>
2024-04-09 19:30:43 -0400

    Updated README

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # simple-rest-api
+
 simple-rest-api
+
+Stored in : https://github.com/hughbrien/simple-rest-api
+
+
+Program written from scratch to figure out GoLang Modules and Profiling
+
+Also need practice with declaring functions with Pointer reference 
+
+Need to details to profiling program. 
+
--- main.go
+++ main.go
@@ -1,17 +1,58 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"net/http"
+	_ "net/http/pprof"
+	"os"
+	"runtime"
+	"runtime/pprof"
 )
 
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
+var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
+
 func main() {
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal("could not create CPU profile: ", err)
+		}
+		defer f.Close() // error handling omitted for example
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
+		defer pprof.StopCPUProfile()
+	}
 	fmt.Println("Starting Server")
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/name", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "{'first_name':'Hugh '")
+	})
+
+	mux.HandleFunc("/serve", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "{'first_name':'serve '")
+	})
+
+	mux.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "{'first_name':'echo '")
 	})
 	if err := http.ListenAndServe("localhost:8090", mux); err != nil {
 		fmt.Println(err.Error())
+	}
+
+	if *memprofile != "" {
+		f, err := os.Create(*memprofile)
+		if err != nil {
+			log.Fatal("could not create memory profile: ", err)
+		}
+		defer f.Close() // error handling omitted for example
+		runtime.GC()    // get up-to-date statistics
+		if err := pprof.WriteHeapProfile(f); err != nil {
+			log.Fatal("could not write memory profile: ", err)
+		}
 	}
 }
